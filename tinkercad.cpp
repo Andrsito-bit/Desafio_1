84ce2620f0c685be3ee03e9ef44e66fa75568155
AndresEsan <152639859+Andrsito-bit@users.noreply.github.com>
2024-09-10 21:19:25 -0500

    Explicacion del codigo

--- tinkercad.cpp
+++ tinkercad.cpp
@@ -1,46 +1,39 @@
-// C++ code
-//
 #include <Adafruit_LiquidCrystal.h>
-int analogPin = 0;
-int val = 0;
-bool confirm = false; 
 
+// Definición de pines
+int analogPin = 0;  // Pin de entrada analógica
+int val = 0;        // Valor de la señal leída
+bool confirm = false;  // Estado del proceso de adquisición
 
-
+// Configuración de la pantalla LCD
 Adafruit_LiquidCrystal lcd_1(0);
 
-void setup()
-{
-  Serial.begin(9600);
+void setup() {
+  Serial.begin(9600);  // Inicialización del monitor serial
   lcd_1.begin(16, 2);  // Configurar LCD de 16x2
-  lcd_1.print("Esperando...");
-
+  lcd_1.print("Esperando...");  // Mensaje inicial en la pantalla
 }
 
-void loop()
-{
+void loop() {
+  lcd_1.setCursor(0, 0);  // Posicionar el cursor en la pantalla LCD
   
-  //lcd_1.clear();
-  lcd_1.setCursor(0, 0);
-  
-  
-  if (digitalRead(3)== HIGH) {
+  // Verificar si el pulsador de inicio (pin 3) está presionado
+  if (digitalRead(3) == HIGH) {
     lcd_1.clear();
-    lcd_1.print("Inicio ");
-    confirm = true;
-    }
-  while (confirm == true) {
-  val = analogRead(analogPin);
-      Serial.println(val);
-    
-    if (digitalRead(5) == HIGH) {
-    lcd_1.clear();
-    lcd_1.print("Pausa ");
-    confirm = false;
-    }
+    lcd_1.print("Inicio ");  // Mostrar mensaje de inicio en la pantalla
+    confirm = true;  // Activar el estado de adquisición
   }
   
-  
-  
-  
+  // Ciclo de adquisición mientras el estado es true
+  while (confirm == true) {
+    val = analogRead(analogPin);  // Leer la señal analógica
+    Serial.println(val);  // Mostrar el valor en el monitor serial
+    
+    // Verificar si el pulsador de pausa (pin 5) está presionado
+    if (digitalRead(5) == HIGH) {
+      lcd_1.clear();
+      lcd_1.print("Pausa ");  // Mostrar mensaje de pausa en la pantalla
+      confirm = false;  // Pausar la adquisición
+    }
+  } 
 }
